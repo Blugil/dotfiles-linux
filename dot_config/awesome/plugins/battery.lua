081cf5e10474a7c8b980fd9e3bc88763eeb966f5
Dominick Agnello <dagnello@asu.edu>
2022-06-15 01:19:16 -0700

    awesome: added awesome config, configured very similarly to bspwm

---
+++ dot_config/awesome/plugins/battery.lua
@@ -0,0 +1,12 @@
+local wibox = require("wibox")
+local awful = require("awful")
+local naughty = require("naughy")
+local watch = require("awful.widget.watch")
+
+
+battery_widget = wibox.widget {
+    {
+        id = "icon",
+
+    }
+}
